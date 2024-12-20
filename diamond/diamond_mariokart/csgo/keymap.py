6c271b886f3213227d210b3e3cedc82bbbfcb2a7
Rafael Irgolic <hello@irgolic.com>
2024-12-20 07:27:18 +0000

    /diamond/diamond_mariokart => /diamond_mariokart

--- diamond/diamond_mariokart/csgo/keymap.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-
-CSGO_KEYMAP = {
-    pygame.K_d: "active_item",
-    pygame.K_RIGHT: "right",
-    pygame.K_LEFT: "left",
-}
--- diamond/diamond_mariokart/game/__init__.py
+++
@@ -1,3 +0,0 @@
-from .game import Game
-from .play_env import PlayEnv
-from .colab_game import ColabGame
